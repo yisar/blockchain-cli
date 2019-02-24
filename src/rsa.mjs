cd0f82e8111445894aa2634798b71caa9f57dad7
132yse <1533540012@qq.com>
2019-02-24 19:09:12 +0800

    日常提交

--- src/rsa.mjs
+++ src/rsa.mjs
@@ -4,8 +4,11 @@ const ec = new elliptic.ec('secp256k1')
 
 let keypair = ec.genKeyPair()
 
-const res = generateKeys()
-console.log(res)
+const keys = generateKeys()
+
+function getPub(prv) {
+  return ec.keyFromPrivate(prv).getPublic('hex').toString()
+}
 
 function generateKeys() {
   const fileName = './src/wallet.json'
@@ -28,6 +31,23 @@ function generateKeys() {
   }
 }
 
-function getPub(prv) {
-  return ec.keyFromPrivate(prv).getPublic('hex').toString()
+function sign({ from, to, amount }) {
+  const bufferMsg = Buffer.from(`${from}-${to}-${amount}`)
+  let sign = Buffer.from(keypair.sign(bufferMsg).toDER()).toString('hex')
+  return sign
 }
+
+function verify({ from, to, amount, sign }, pub) {
+  const keypairTemp = ec.keyFromPublic(pub, 'hex')
+  const bufferMsg = Buffer.from(`${from}-${to}-${amount}`)
+  return keypairTemp.verify(bufferMsg, sign)
+
+}
+
+const trans = { from: 'yse', to: '132', amount: 100 }
+const signa = sign(trans)
+console.log(signa)
+trans.sign = signa
+
+const is = verify(trans, keys.pub)
+console.log(is)
